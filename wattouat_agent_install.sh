92eb9d723196e6505ee57373e0f35bd745feeeaf
czantoine <ac11f@free.fr>
2022-03-12 11:12:21 +0100

    debug installation

--- wattouat_agent_install.sh
+++ wattouat_agent_install.sh
@@ -18,16 +18,14 @@ cp wattouat_agent.service /etc/systemd/system/
 cp wattouat_agent_service.sh /bin/wattouat_agent
 chmod +x /bin/wattouat_agent/wattouat_agent_service.sh
 
-# Function
-cp -R fonction /bin/wattouat_agent/
-
-bash /bin/wattouat_agent/fonction/mysql/node_info.sh
-bash /bin/wattouat_agent/fonction/mysql/cpu.sh
-
 # Config file
-
+cp -R fonction /bin/wattouat_agent/
 cp wattouat_agent.conf /bin/wattouat_agent/
 cp cpu_proc.txt /bin/wattouat_agent/
+
+# Function
+bash /bin/wattouat_agent/fonction/mysql/node_info.sh
+bash /bin/wattouat_agent/fonction/mysql/cpu.sh
 
 echo ""
 echo "/******************************************************/"
